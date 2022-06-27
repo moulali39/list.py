2c0ddd2aecb58fc5915b68b8d33ff61bf2274f93
moulali39 <107177595+moulali39@users.noreply.github.com>
2022-06-27 19:09:13 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,86 @@
+# a=['moulali',44, 'true' ,'hii','none']
+# print(type(a))
+# print((a[0]))
+# print(a[3])
+# print(a[1])
+# print(a[1])
+# # print(a[5])
+
+# a=['moulali','friens','college',100,['1','2','3']]
+# # a[2]="pushpalatha"
+# print(a[-1])
+# print(a[-2])
+# print(a[-3])
+# print(a[::-1])
+# print(a[-1][-2])
+# print(a[0][2])
+# print(a[2][3])
+
+
+# x=[5,7]
+# print(x)
+# print(x[0])
+# # print(x[-1]) 
+
+# a=['hiii',143,' hyderabad']
+# a.append(6)
+# print(a)
+
+# a=['hiii',143,' hyderabad']
+# a.clear()
+# print(a)
+
+
+# a=['hiii',143,'hyderabad','hyderabad']
+# print(a.count('hyderabad'))
+
+# a=['moulali'+"❤ "+ 'love']
+# b='143'
+# a.extend(b)
+# # print(a)
+# a=[1,2,3,4,5]
+# b=[2,3,4,5] 
+# a.extend(b)
+# # print(a)
+
+# a=[1,2,3,4,5, 'moulali',[6.7]]
+# print(a.index[6.7])
+# print(a)
+
+# a=[1,2,3,4,]
+# a.insert(2,'love')
+# print(a)
+
+# x=['moulali','vahida',3,5,'hyderabad']
+# x.insert(3,889)
+# print(x)
+
+
+# x=['moulali','vahida',3,5,'hyderabad']
+# print(x.pop())
+# print(x)
+# a=[40,50,60,70]
+# print(a.pop())
+# print(a)
+
+# x=['moulali','vahida',3,5,'hyderabad']
+# x.append(3)
+# print(x)
+
+# x=['moulali','vahida',3,5,'hyderabad']
+# x.remove('vahida')
+# print(x)
+
+
+# s=[1,2,3,4,5,6]
+# print(s[::-1])
+# a='i love my country'
+# print(a[::-1])
+
+# a=[2,8,6,9,0,5.6,2.4]
+# a.sort()
+# print(a)
+
+print(chr(65))
+print(chr(80))
+print(chr(90))
